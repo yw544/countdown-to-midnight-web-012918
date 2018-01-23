c7a4db188910410728a32e4ddd2e8451bb076044
York Wang <yw544@nyu.edu>
2018-01-23 02:24:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ end
 return "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(time)
+  while time > 0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
+    
